2668ce4a26ff5bece6c9dc2098d4d3dad9b72967
Sruti Samatkar <72342064+ssamatkar@users.noreply.github.com>
2023-04-14 11:39:40 +0530

    Create instance.tf

---
+++ Lab 3/instance.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = var.AWS_REGION
+}
+
+resource "aws_instance" "example" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips.txt"
+  }
+}
+
+
+
