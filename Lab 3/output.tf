20d83a011ac2003333d2c2ac65af5adf40770f9f
Sruti Samatkar <72342064+ssamatkar@users.noreply.github.com>
2023-04-14 11:41:33 +0530

    Create output.tf

---
+++ Lab 3/output.tf
@@ -0,0 +1,12 @@
+output "Public_ip" {
+  description = "Public IP of the instance"
+  value = aws_instance.example.public_ip
+}
+
+output "Private_ip" {
+  sensitive = true
+  description = "Private IP of the instance"
+  value = aws_instance.example.private_ip
+
+}
+
